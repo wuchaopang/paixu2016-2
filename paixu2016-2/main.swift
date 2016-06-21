cbce9067fab4267583512584d8093baae7e7836d
wuchaopang <20141105049wch@mac23.local>
2016-06-21 11:11:39 +0800

    Initial Commit

---
+++ paixu2016-2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  paixu2016-2
+//
+//  Created by 20141105049 on 16/6/21.
+//  Copyright © 2016年 20141105049. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
